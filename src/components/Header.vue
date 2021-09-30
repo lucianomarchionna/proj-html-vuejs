6819100ec4c04e7662b11166e362b3c0781bb9e4
Luciano <lucco6823@gmail.com>
2021-09-30 16:24:17 +0200

    Aggiunta all'header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,17 @@
 <template>
   <header class="container">
     <div class="header-top">
+      <div class="header-top-left">
+        <h6>Download App <div class="ios">iOS</div>Advertise with us</h6>
+      </div>
+      <div class="header-top-right">
+        <i class="fab fa-facebook-f"></i>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-youtube"></i>
+      </div>
+    </div>
+    <div class="header-between">
       <img class="center" src="../assets/img/avada-food-logo.png" alt="Logo Avada Food">
     </div>
     <div class="header-bottom">
@@ -29,12 +40,35 @@ export default {
 @import "../style/generals";
 .container{
   width: 100%;
-  height: 150px;
+  height: 170px;
   display: flex;
   flex-direction: column;
   align-items: center;
 
   .header-top{
+    display: flex;
+    width: 60%;
+    height: 20px;
+    justify-content: space-between;
+    color: $color-cube-3;
+    cursor: pointer;
+    
+
+    .ios{
+      display: inline-block;
+      text-align: center;
+      vertical-align: middle;
+      padding: 2px 6px;
+      color: $mediam-cut-2;
+      border-radius: 5px;
+      background-color: $color-cube-5;
+      margin-right: 10px;
+    }
+    i{
+      margin-right: 20px;
+    }
+  }
+  .header-between{
     height: 90px;
 
     img{
